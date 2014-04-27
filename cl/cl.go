f34c3c2eac01917ff78355b855c8cdc1cd19b23e
Cyril Deguet <cyril.deguet@gmail.com>
2014-04-27 10:59:03 +0200

    Fixed build on MacOS X

--- cl/cl.go
+++ cl/cl.go
@@ -16,7 +16,11 @@ in the Go standard packages.
 */
 package cl
 
-// #include "cl.h"
+// #ifdef __APPLE__
+// #include <OpenCL/opencl.h>
+// #else
+// #include <CL/opencl.h>
+// #endif
 // #cgo darwin LDFLAGS: -framework OpenCL
 import "C"
 import "errors"
